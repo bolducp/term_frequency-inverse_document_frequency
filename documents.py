c5bcc3f9f0a1a879fe2b2a3679b01846d4d5387a
Paige Finkelstein <paigelfink@gmail.com>
2019-08-02 15:56:59 -0400

    initial tf_idf implementation

---
+++ documents.py
@@ -0,0 +1,90 @@
+july_jobs_report = """July Jobs Report: U.S. Added 164,000 Jobs; Unemployment Unchanged
+The figures suggest the economy is cooling but employers are still creating jobs at a solid pace.
+The Takeaway
+Just two days after the Federal Reserve moved to ward off economic snags by paring interest rates, the monthly jobs report signals that the labor market is maintaining its energy during a record-long hiring streak.
+
+While last month's payroll additions did not match gains in June, July's numbers show that employers continue to make room for new job seekers.
+
+"Job gains were fairly broad-based across different sectors," said Gregory Daco, chief economist of Oxford Economics USA. "This was a good jobs report overall."
+
+Restaurants, business services, health care and education all posted solid gains. And the manufacturing sector, which had been slowing, created 16,000 jobs.
+
+The tight job market is continuing to pull in workers from the sidelines, and the labor force participation rate, which has not yet returned to pre-recession levels, did tick up. A broader measure of unemployment that includes part-timers who would prefer to work full-time and those too discouraged to even look for a job dropped to 7 percent, the lowest rate since 2000."""
+
+us_china_treaty = """U.S. Ends Cold War Missile Treaty, With Aim of Countering China
+Trump administration officials say that the treaty tied their hands on China and that Russia was not complying with it, but its demise raised fears of a new arms race.
+WASHINGTON — The United States on Friday terminated a major treaty of the Cold War, the Intermediate Nuclear Forces agreement, and it is already planning to start testing a new class of missiles later this summer.
+
+But the new missiles are unlikely to be deployed to counter the treaty's other nuclear power, Russia, which the United States has said for years was in violation of the accord. Instead, the first deployments are likely to be intended to counter China, which has amassed an imposing missile arsenal and is now seen as a much more formidable long-term strategic rival than Russia.
+
+The moves by Washington have elicited concern that the United States may be on the precipice of a new arms race, especially because the one major remaining arms control treaty with Russia, a far larger one called New START, appears on life support, unlikely to be renewed when it expires in less than two years.
+
+At a moment when the potential for nuclear confrontations with North Korea and Iran is rising, the American decision to abandon the 32-year-old treaty has prompted new worries in Europe and Asia, and warnings that echo an era that once seemed banished to the history books. The resurgence of nuclear geopolitics was evident in the Democratic debate on Tuesday night, when presidential hopefuls grappled with whether the United States should renounce "first use" of nuclear weapons in any future conflict.
+
+"The United States and Russia are now in a state of strategic instability," Ernest J. Moniz, the former energy secretary, and Sam Nunn, the former Georgia senator who helped draft the legislation that funded the drastic reduction in former Soviet nuclear forces, write in a coming article in Foreign Affairs ominously titled "The Return to Doomsday." "Not since the 1962 Cuban missile crisis has the risk of a U.S.-Russian confrontation involving the use of nuclear weapons been as high as it is today. Yet unlike during the Cold War, both sides seem willfully blind to the peril."
+
+Others are less concerned about the implications with Russia, noting that the treaty was limited, covering only a narrow class of missiles."""
+
+millenials_playboy = """Will the Millennials Save Playboy?
+The Hefners are gone, and so is the magazine's short-lived ban on nudity — as well as virtually anyone on the staff over 35.
+LOS ANGELES — "This is a full nude shoot to be conducted underwater," a woman said into a phone, her voice carrying across a row of cubicles.
+
+"Right, that's right," she continued. The person on the other end didn't seem to quite get it. "As in, it won't be on land."
+
+It was a Tuesday morning at the Westwood headquarters of Playboy Enterprises,, and editors were preparing to close their summer issue. Gathered between a velvet love seat and a view of Santa Monica, they discussed upcoming stories — a piece on B.D.S.M., a profile of Pete Buttigieg — while in the kitchen, a barista stenciled bunny ears into latte foam.
+
+In his office, Shane Singh, Playboy's executive editor, explained that the underwater photo shoot, to be photographed that weekend, was for the magazine's cover — but not in the way that older, leering readers might expect.
+
+"The water is meant to represent gender and sexual fluidity," Mr. Singh said, seated beneath a 1988 Herb Ritts portrait of Cindy Crawford. The women who would pose in that water — their limbs wrapped around one another in a balletlike pose — were not simply models but activists. One uses performance art and digital media to share stories about the H.I.V. epidemic. Another is an underwater dancer who promotes ocean conservation. The third, a Belgian artist, recently filmed herself walking naked through a Hasidic neighborhood of Brooklyn during a sacred holiday. (An angry mob chased her out.)
+
+"When they called me for this shoot, I thought there must be some kind of mistake," Ed Freeman, the fine art photographer who created the cover image, later told me. "I hadn't paid attention to Playboy for many years, since I was a kid. And I thought: 'Wait, they're hiring me to shoot the cover? Do they know I'm gay?'"
+
+This is a newer, woke-er, more inclusive Playboy — if you believe what company executives tell you, and if you are inclined to give an aging brand yet another chance at reinvention.
+
+Even before the #MeToo movement, there had long been debate over whether a publication with the tag line "Entertainment for Men" had any place in an equitable world. But when Playboy's founder, Hugh Hefner, died in 2017, that argument grew louder: Had Mr. Hefner been a forward-thinking voice for sexual liberation and free speech, or a creepy old lech who fostered a culture of misogyny?"""
+
+modern_love = """I Had to Know if He Was Seeing Someone Else
+As the heartbroken mother of two young girls, I found myself routinely — compulsively — checking up on my ex.
+The Palmetto Expressway, known locally as "The 826," which bypasses the center of Miami-Dade County, is about 30 miles long. Decades ago, I was only concerned with the nearly six-mile stretch between the exits for Kendall Drive and West Flagler Street, the highway distance between my home and the home of the man who had broken up with me.
+
+His stated reason was that he couldn't be sure of ever wanting a family. Meaning he didn't want to be absorbed into mine with my two daughters: Vanessa, 7, who was born worried, and Veronica, 4, a people pleaser who would give me shoulder massages ("machaches") and say, "Am I making you better, Mommy? Am I doing it good?"
+
+We had come from Puerto Rico only three years before, when my girls were 4 and 1 and I was 25. I had been offered a coveted news internship in Atlanta that quickly turned into a job there which then led to a better job in Miami. As a single mother, I had been warned repeatedly about the difficulty of finding love with a man who was open to a long-term relationship, but at first he had seemed like he might be that man."""
+
+beach_house = """Looking for a Beach House? It'll Cost You
+These days, you may end up paying more than just the asking price for a home near the water.
+In the first century B.C., the Roman poet Horace wrote, "Reason and sense remove anxiety/Not villas that look out upon the sea." Two thousand years later — reasonable or not — people are still paying top dollar for waterfront homes, attracted by hypnotic waves, briny top notes and deep-blue horizons.
+
+Even with the effects of climate change turning coastal properties into cause for anxiety, rather than an antidote, there remains a strong demand. At least that's the consensus of five real estate brokers interviewed for this special "What You Get" column devoted to beach houses.
+
+"The stock market's doing well; there is more discretionary income for people who want to realize the dream of a tropical getaway," said Jeffrey Burns, an agent at Premier Sotheby's International Realty on Captiva Island, a sliver of land off the Florida Gulf Coast.
+
+"We don't see people being paranoid about buying," said Mark Fisher, the manager of Coldwell Banker Schmidt Realty's office in Glen Arbor, Mich., which is wedged between a great lake and two smaller ones, and where some homeowners have watched rising water levels submerge their beaches bit by bit.
+
+"Waterfront is always desirable, and there are always risks associated with it, whether you believe in climate change or not," said Doug Coby of Rescue Dog Realty, in southern Connecticut. "People always want to be near the beach."""
+
+skating_sexual_assault = """Ashley Wagner's Account of Sexual Assault Shakes Figure Skating
+The Olympian Ashley Wagner on Thursday said an older male figure skater had sexually assaulted her when she was 17, shining another harsh light on the sport. It is one of beauty and elegance but also of extreme pressure, with athletes of different ages often placed together while under little supervision by parents or officials.
+
+Wagner's allegations were the second public accusation of assault against John Coughlin, who died by suicide in January. The accusations have further raised concerns that the dynamics of figure skating feed a culture in which young women are all too vulnerable.
+
+"It's the equivalent of a senior prom all the time," said Debbie Varner, 58, a former skating coach.
+
+Wagner, 28, is a three-time United States skating champion who has won medals at the Olympics and the world championships. Coughlin, a former national pairs champion, killed himself in January while facing an investigation into accusations of sexual misconduct, including inappropriate conduct involving minors."""
+
+aging_pet = """How to Care for an Aging Pet
+Older dogs and cats require a little extra love and care, whether they're new to your home or they've been with you for years. Here's what you need to know.
+I have dogs. I have four dogs. I have four dogs in New York City.
+
+In my defense, it happened by accident. See, I had two and my husband had two, so we Brady Bunch-ed them when we got married. I love them equally but this piece will focus on the eldest, Gixer, who's 10½. (I may refer to him here by one of his many nicknames: Jiggy, JigPig, Gixy, Jiggy Pie, Jiggus McPiggus, Jiggus M. Piggus Attorney at Dog, Bird, Turd, Bubba. He might have a couple more by the time I finish writing.)
+
+A few weeks ago, I was in the park with Gixer when a man and his young daughter walked by. "That dog looks so old," the man said as he passed. I felt a pang in my chest. Despite my best efforts to freeze time, my boy is aging and it shows: his beard and eyebrows have gone white, he moves with arthritic stiffness, he's got a bum knee that causes him to limp sometimes. His eyes are a bit cloudy, though his vision is still good, and he has a few fatty lumps just below the skin — benign lipomas that come with age. His anxiety, especially at nighttime, has increased, and his unpredictable grumpiness is directed at everything from skateboards to shopping bags."""
+
+new_york_farm_stand = """The Greenmarket go-to for high-end chefs is run by an upstate couple who custom-grow new varieties, using labor and techniques from Egypt.NORWICH, N.Y. — At first glance, the Norwich Meadows Farm stand at the Union Square Greenmarket in Manhattan looks like any other, with its rows of onions and lettuce, and a banner with the words “fresh" and “organic" in big green letters.
+
+But there's something the chefs who regularly crowd the stand know that other customers may not: Its owners, Zaid and Haifa Kurdieh, have an uncanny ability to spot the next big thing in produce and grow it in bulk.
+
+Their farm here in Norwich, a former manufacturing town about 60 miles southeast of Syracuse, is a prime reason that Jimmy Nardello peppers and Persian cucumbers have become ubiquitous in New York City restaurants.The Kurdiehs provide Michael Anthony, the chef of Gramercy Tavern, with habanada peppers, a mild variant of the habanero. They sell sweet Kyoto red carrots to the chef Derek Wilcox of the Japanese restaurant Shoji at 69 Leonard Street, and tomatillo-like husk cherries to the chefs Jeremiah Stone and Fabian von Hauske Valtierra of Wildair and Contra. Their produce also pops up at Eleven Madison Park, Blue Hill and Momofuku Ko."""
+
+
+documents = [ july_jobs_report, us_china_treaty,  millenials_playboy,  modern_love, beach_house, skating_sexual_assault, aging_pet, new_york_farm_stand ]
---
+++ tf_idf.py
@@ -0,0 +1,82 @@
+import string, math
+import numpy as np
+from documents import documents
+
+# TF-IDF(term) = TF(term in a document) * IDF(term)
+# TF(term) = # of times the term appears in document / total # of terms in document
+# IDF(term) = log(total # of documents / # of documents with term in it)
+
+def parse(doc):
+    return doc.translate(str.maketrans("", "", string.punctuation))
+
+
+def create_vocab_map():
+    vocabulary = list(set("".join(parsed_documents).split()))
+    return { word: index for index, word in enumerate(vocabulary) }
+
+def create_index_to_vocab_map(vocab_map):
+    return { value: key for key, value in vocab_map.items() }
+
+def create_word_freq_array(document, vocab_map):
+    vocabulary = vocab_map.keys()
+    word_freqs = np.zeros(len(vocabulary))
+    doc_total_word_count = len(document.split())
+
+    for word in set(document.split()):
+        if word in vocabulary:
+            word_freqs[vocab_map[word]] += calculate_term_freq(word, document, doc_total_word_count)
+    return word_freqs    
+
+
+def calculate_term_freq(word, document, doc_total_word_count):
+    word_count = document.split().count(word)
+    return word_count / doc_total_word_count
+
+
+def create_idf_array(vocab_map, documents):
+    document_count = len(documents)
+    vocabulary = vocab_map.keys()
+    word_freqs = np.zeros(len(vocabulary))
+
+    for word in vocabulary:
+        docs_with_word = (len(list(filter(lambda x: word in x, documents))))
+        if not docs_with_word == 0:
+            word_freqs[vocab_map[word]] = math.log(document_count / docs_with_word)
+
+    return word_freqs
+
+
+def calculate_tf_idf(vocab_map, term_frequencies, inverse_document_frequencies):
+    vocabulary = vocab_map.keys()
+    tf_idf = np.zeros(len(vocabulary))
+
+    for word in vocabulary:
+        index = vocab_map[word]
+        tf_idf_value = term_frequencies[index] * inverse_document_frequencies[index]
+        tf_idf[index] = tf_idf_value
+
+    return tf_idf
+
+
+parsed_documents = [parse(doc) for doc in documents]
+vocab_map = create_vocab_map()
+index_to_vocab_map = create_index_to_vocab_map(vocab_map)
+idf = create_idf_array(vocab_map, parsed_documents)
+
+all_docs_td_idfs = np.array([calculate_tf_idf(vocab_map, create_word_freq_array(parse(document), vocab_map), idf) for document in parsed_documents])
+
+
+def top_10_words(doc):
+    tf_idf = calculate_tf_idf(vocab_map, create_word_freq_array(parse(doc), vocab_map), idf)
+    indexes = np.argpartition(tf_idf, -10)[-10:]
+    indexes = indexes[np.argsort(tf_idf[indexes])][::-1]
+
+    top_10_tf_idf_values = [tf_idf[i] for i in indexes]
+    top_10_tf_idf_words = [index_to_vocab_map[i] for i in indexes]
+
+    tf_idf_scores_and_words = np.array(list(zip(top_10_tf_idf_values, top_10_tf_idf_words)))
+
+    return tf_idf_scores_and_words
+
+
+print(top_10_words(parsed_documents[0]))
